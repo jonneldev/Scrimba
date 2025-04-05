af2be37737c49eb4f51c1d30a3a514d82eb3e695
nada0715 <jonneldosado.main@outlook.com>
2025-04-06 05:55:00 +0800

    Tenzies Game

--- dev/Tenzies/App.jsx
+++ dev/Tenzies/App.jsx
@@ -6,23 +6,6 @@ import Confetti from "react-confetti"
 export default function App() {
     const [dice, setDice] = useState(() => generateAllNewDice())
     
-    /**
-     * Challenge:
-     * Make it so when the game is over, the "New Game" button
-     * automatically receives keyboard focus so keyboard users
-     * can easily trigger that button without having to tab
-     * through all the dice first.
-     * 
-     * Hints:
-     * 1. Focusing a DOM element with the DOMNode.focus() method
-     *    requires accessing the native DOM node. What tool have
-     *    we learned about that allows us to do that?
-     * 
-     * 2. Automatically calling the .focus() on a DOM element when
-     *    the game is won requires us to synchronize the local
-     *    `gameWon` variable with an external system (the DOM). What
-     *    tool have we learned about that allows us to do that?
-     */
 
     const gameWon = dice.every(die => die.isHeld) &&
         dice.every(die => die.value === dice[0].value)
@@ -31,8 +14,7 @@ export default function App() {
         return new Array(10)
             .fill(0)
             .map(() => ({
-                // value: Math.ceil(Math.random() * 6),
-                value: 5,
+                value: Math.ceil(Math.random() * 6),
                 isHeld: false,
                 id: nanoid()
             }))
