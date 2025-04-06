e560e259727297a933c796ac865e6e8f73863afa
nada0715 <jonneldosado.main@outlook.com>
2025-04-06 09:32:51 +0800

    chef claude

--- dev/chef-claude/ai.js
+++ dev/chef-claude/ai.js
@@ -6,7 +6,7 @@ You are an assistant that receives a list of ingredients that a user has and sug
 
 // Make sure you set an environment variable in Scrimba 
 // for HF_ACCESS_TOKEN
-const hf = new HfInference(process.env.VITE_HF_ACCESS_TOKEN)
+const hf = new HfInference(import.meta.env.VITE_HF_ACCESS_TOKEN)
 
 export async function getRecipeFromMistral(ingredientsArr) {
     const ingredientsString = ingredientsArr.join(", ")
